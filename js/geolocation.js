f39b2b310885059829185df675d74f7751882d17
DIOGENES JUNIOR <contato@diogenesjunior.com.br>
2017-01-29 15:47:33 -0200

    Novos testes
    Novos testea

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,13 +1,14 @@
 document.addEventListener("deviceready", function() {
 
     var onSuccess = function(position) {
-        $("#latitude").val(position.coords.latitude);
-        $("#longitude").val(position.coords.longitude);
-        $("#altitude").val(position.coords.altitude);
+        $("#dfLatitude").val(position.coords.latitude);
+        $("#dfLongitude").val(position.coords.longitude);
+        
+        /*$("#altitude").val(position.coords.altitude);
         $("#accuracy").val(position.coords.accuracy);
         $("#altitudeAccuracy").val(position.coords.altitudeAccuracy);
         $("#heading").val(position.coords.heading);
-        $("#speed").val(position.coords.speed);
+        $("#speed").val(position.coords.speed); */
         $("#timestamp").val(position.timestamp);
 
         $.mobile.loading("hide");
